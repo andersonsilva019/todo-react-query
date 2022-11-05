a49719d1fb4827f755c57e1b98a7cc4e7fd871ba
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-11-05 16:38:15 -0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-export function App() {
+import { Routes, Route } from 'react-router-dom'
+import { Todos } from './pages/Todos'
+import { Todo } from './pages/Todo'
+import { GlobalStyles } from './styles/global'
 
+export function App() {
   return (
-    <h1>Hello</h1>  
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Todos />} />
+        <Route path="/todo/:id" element={<Todo />} />
+      </Routes>
+    </>
   )
 }
